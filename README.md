498b2f1b7a9d7b05e96c70ebe2f6153c5962602a
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-06-26 19:13:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clock
+
+Clock is a very important thing in our lives so I built a simple digital clock based on the person's current time using pure javascript with a basic layout
---
+++ main.js
@@ -0,0 +1,16 @@
+function AddClock() {
+    const now = new Date()
+    const hours = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
+
+    if(seconds < 0){
+        seconds = "0" + seconds
+    }
+
+    let clock = `${hours} : ${minutes} : ${seconds}`
+
+    document.getElementById('Clock').innerHTML = clock
+}
+
+setInterval(AddClock, 1000)
